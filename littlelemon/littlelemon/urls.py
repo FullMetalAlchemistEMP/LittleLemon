ff2b254fe4fae14f0731deb87c5f69b0d865a61c
FullMetalAlchemistEMP <fullmetalalchemistemp@gmail.com>
2025-06-26 16:18:21 +0300

    fixes

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from restaurant import views
 from django.shortcuts import redirect
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
@@ -31,4 +32,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token),
 ]
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin 
 from django.urls import path 
 from . import views 
-from rest_framework.authtoken.views import obtain_auth_token
+
   
 urlpatterns = [
     # HTML views
@@ -10,5 +10,4 @@ urlpatterns = [
     # REST API views
     path('items/', views.MenuItemsView.as_view(), name="api-menu"),
     path('items/<int:pk>/', views.SingleMenuItemView.as_view(), name="api-single-menu"),
-    path('api-token-auth/', obtain_auth_token),
 ]
